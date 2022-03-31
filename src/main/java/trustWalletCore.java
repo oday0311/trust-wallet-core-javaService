b8913cb5fde1dac36fc69bb7d2397c54997fdec4
phenix <phenix@fancy.com>
2022-03-31 16:56:14 +0800

    init, but not working with maven

---
+++ src/main/java/trustWalletCore.java
@@ -0,0 +1,30 @@
+
+import wallet.*;
+public class trustWalletCore {
+
+
+    //https://github.com/trustwallet/wallet-core/tree/master/samples/android .
+    //https://developer.trustwallet.com/wallet-core/integration-guide/android-guide#account-address-derivation
+
+    {
+        System.loadLibrary("TrustWalletCore");
+    }
+
+    public static void main(String [] args)
+    {
+
+
+        System.out.println("hello ar");
+    }
+
+
+    public static void createWallet()
+    {
+
+        {
+            System.loadLibrary("TrustWalletCore");
+        }
+        //HDWallet()
+    }
+
+}
